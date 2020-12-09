d7a120773455539fb03c9d3d724453e23161b510
jgraykeyin <justin.gray@keyin.com>
2020-12-09 12:56:06 -0330

    Added first label to the window

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import tkinter as tk
 
+# Initialize our main window, huzzah!
+window = tk.Tk()
+
+# Set the title of our window
+window.title("Bacteria Culture-tron")
+
+# Setup our first label
+cultureid_label = tk.Label(window, text="Culture ID: ")
+
+# Stick that label into the grid!
+cultureid_label.grid(row=1, column=1)
+
+window.mainloop()
+
+
